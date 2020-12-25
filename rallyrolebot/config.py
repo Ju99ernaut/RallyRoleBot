2a9bcefeeb46df8ba143984546d13dd53969ad7f
Ju99ernaut <brendonzee@gmail.com>
2020-12-25 16:37:32 +0200

    Get default token from env

--- rallyrolebot/config.py
+++ rallyrolebot/config.py
@@ -1,12 +1,16 @@
+import os
 import configargparse
 
 CONFIG = None
+secret_token = os.getenv("SECRET_KEY")
 
 arg_parser = configargparse.ArgParser(default_config_files=["config.txt"])
 
 arg_parser.add("-c", "--config", is_config_file=True, help="Config file")
 
-arg_parser.add("-t", "--secret_token", help="Discord bot login token")
+arg_parser.add(
+    "-t", "--secret_token", default=secret_token, help="Discord bot login token"
+)
 
 arg_parser.add(
     "-p", "--command_prefix", default="$", help="The symbol used before commands"
